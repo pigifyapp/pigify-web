07017afd1dbfcee1f2c5bbbd01b834a9e72cec2a
andresbytes <giantquartz@gmail.com>
2023-02-12 15:38:43 +0100

    Show balance and goal

--- src/pigify.js
+++ src/pigify.js
@@ -1,6 +1,6 @@
 import abi from "./abi.json";
 import web3 from "./web3";
 
-const contract = new web3.eth.Contract(abi, "0x476353473f7b173ff47ca75aa2585bfb26c7d4af");
+const contract = new web3.eth.Contract(abi, "0x52a431B6CF188Ef00E99F3e049B30c06D0324165");
 
 export default contract;
--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,33 @@
 import pigify from "../pigify";
 import { useEffect, useState } from "react";
+import Piggybank from "../components/Piggybank";
+import web3 from "../web3";
 
 const App = function() {
-    const [balance, setBalance] = useState(0);
+    const [address, setAddress] = useState("Unknown address");
+    const [freePGY, setFreePGY] = useState("");
+
+    const [balanceUSDT, setBalanceUSDT] = useState(0);
+    const [balancePGY, setBalancePGY] = useState(0);
+    const [goalUSDT, setGoalUSDT] = useState(0);
+    const [goalPGY, setGoalPGY] = useState(0);
 
     const update = async() => {
-        setBalance(await pigify.methods.balanceOf("0xeC357C27a26E94955eb415633404495044Ba7fcd").call())
+        const accounts = await web3.eth.getAccounts();
+
+        const targetAddress = accounts[0];
+
+        setAddress(targetAddress);
+
+        const pgy = await pigify.methods.balanceOf(targetAddress).call();
+        
+        setFreePGY((pgy / 10 ** 18).toLocaleString("en-US"));
+
+        setBalanceUSDT(await pigify.methods.balanceUSDT(targetAddress).call());
+        setGoalUSDT(await pigify.methods.balancePGY(targetAddress).call());
+
+        setBalancePGY(await pigify.methods.balancePGY(targetAddress).call());
+        setGoalPGY(await pigify.methods.goalPGY(targetAddress).call())
     }
 
     useEffect(() => {
@@ -14,9 +36,12 @@ const App = function() {
 
     return (
         <div>
-            <p>Your balance: {balance}</p>
-            <p>USDT savings: 0</p>
-            <p>PGY savings: 0</p>
+            <p>Your address: {address}</p>
+            <p>Your PGY: {freePGY}</p>
+            <hr />
+            <Piggybank coinName="PGY" balance={balancePGY} goal={goalPGY}/>
+            <hr />
+            <Piggybank coinName="USDT" balance={balanceUSDT} goal={goalUSDT}/>
         </div>
     );
 }
