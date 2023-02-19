ad2b766a51aefa897bbf0d6100bb09e45a2a5ce1
andresbytes <giantquartz@gmail.com>
2023-02-19 17:15:54 +0100

    BankEntry: Add set goal button and modal

--- src/components/BankEntry.js
+++ src/components/BankEntry.js
@@ -4,6 +4,7 @@ import BankEntryModal from "./BankEntryModal";
 const BankEntry = function({tokenName, balance, goal, rewards, image}) {
     const [isDepositModalOpen, setDepositModalOpen] = useState(false);
     const [isWithdrawModalOpen, setWithdrawModalOpen] = useState(false);
+    const [isGoalModalOpen, setGoalModelOpen] = useState(false);
 
     function openDepositModal() {
         setDepositModalOpen(true);
@@ -21,6 +22,14 @@ const BankEntry = function({tokenName, balance, goal, rewards, image}) {
         setWithdrawModalOpen(false);
     }
 
+    function openGoalModal() {
+        setGoalModelOpen(true);
+    }
+
+    function closeGoalModal() {
+        setGoalModelOpen(false);
+    }
+
     return (
         <tr>
             <td className="has-text-centered"><img src={image} alt="Token" width="18px" height="18px"/></td>
@@ -30,6 +39,7 @@ const BankEntry = function({tokenName, balance, goal, rewards, image}) {
             <td>{rewards} PGY</td>
             <td><a onClick={openDepositModal}>Deposit</a></td>
             <td><a onClick={openWithdrawModal}>Withdraw</a></td>
+            <td><a onClick={openGoalModal}>Set goal</a></td>
 
             <BankEntryModal isActive={isDepositModalOpen} title="Deposit" close={closeDepositModal} image={image} balance={balance} tokenName={tokenName} goal={goal} rewards={rewards}>
                 <p className="subtitle">How much would you like to deposit?</p>
@@ -38,6 +48,15 @@ const BankEntry = function({tokenName, balance, goal, rewards, image}) {
 
             <BankEntryModal isActive={isWithdrawModalOpen} title="Withdraw" close={closeWithdrawModal} image={image} balance={balance} tokenName={tokenName} goal={goal} rewards={rewards}>
                 <p className="subtitle">How much would you like to withdraw?</p>
+                <input className="input" type="number" defaultValue="1" />
+            </BankEntryModal>
+
+            <BankEntryModal isActive={isGoalModalOpen} title="Set goal" close={closeGoalModal} image={image} balance={balance} tokenName={tokenName} goal={goal} rewards={rewards}>
+                <p className="subtitle">
+                    <span style={{ color: "red" }}><i className="fa-solid fa-triangle-exclamation"></i> </span>
+                    Be careful, once you set a goal you won't be able to withdraw your money until you save enough tokens.
+                </p>
+                <p className="subtitle">How much do you want to set as goal?</p>
                 <input className="input" type="number" defaultValue="1" />
             </BankEntryModal>
         </tr>
