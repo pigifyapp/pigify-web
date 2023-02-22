0173c97e5a91a6077769ccc1f938be7d9f18a330
andresbytes <giantquartz@gmail.com>
2023-02-22 05:22:47 +0100

    Add new abi and contract address

--- src/web3/pigify.js
+++ src/web3/pigify.js
@@ -1,6 +1,6 @@
 import abi from "./abi.json";
 import web3 from "./web3";
 
-const contract = new web3.eth.Contract(abi, "0xD66EBBCA76fB35C39289DfA8f2D15E7BB1360868");
+const contract = new web3.eth.Contract(abi, "0x7A0C2de46B3E43aF2B86B241932bEA2533e93d13");
 
 export default contract;
