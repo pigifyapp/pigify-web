7d3c1241d639fae32b91170284809deb35334dc4
andresbytes <giantquartz@gmail.com>
2023-02-24 06:57:16 +0100

    Point to BSC contract address

--- src/web3/pigify.js
+++ src/web3/pigify.js
@@ -1,4 +1,4 @@
 import abi from "./abi.json";
 import web3 from "./web3";
-export const pigifyAddress = "0x7A0C2de46B3E43aF2B86B241932bEA2533e93d13";
+export const pigifyAddress = '0x5eF13bA48d3d3766d0B6d9C93e09d80057dcf562';
 export const pigifyContract = new web3.eth.Contract(abi, pigifyAddress);
