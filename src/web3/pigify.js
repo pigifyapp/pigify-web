54fcd2e02378e2aea1a38a8f04d158d0a32d2602
andresbytes <giantquartz@gmail.com>
2023-02-25 15:59:10 +0100

    pigify: Set mainnet smart contract address

--- src/web3/pigify.js
+++ src/web3/pigify.js
@@ -1,4 +1,4 @@
 import abi from "./abi.json";
 import web3 from "./web3";
-export const pigifyAddress = '0x5eF13bA48d3d3766d0B6d9C93e09d80057dcf562';
+export const pigifyAddress = '0xA9BbD56541eAB22A1E256ed47324A9051E965c0b';
 export const pigifyContract = new web3.eth.Contract(abi, pigifyAddress);
