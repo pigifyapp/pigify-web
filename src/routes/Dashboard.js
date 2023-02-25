9efc0cf8fd57272d40f2d766387af5d01a99ea29
andresbytes <giantquartz@gmail.com>
2023-02-25 20:24:38 +0100

    Added BUSD, USDD and DAI

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,6 +1,10 @@
 import BankEntry from "../components/dashboard/BankEntry";
+import PGY from "../images/PGY.png"
 import USDC from "../images/USDC.png";
 import USDT from "../images/USDT.png";
+import BUSD from "../images/BUSD.png";
+import USDD from "../images/USDDD.png";
+import DAI from "../images/DAI.png";
 import {useEffect, useState} from "react";
 import Navbar from "../components/dashboard/Navbar";
 import web3 from "../web3/web3";
@@ -44,9 +48,12 @@ export default function Dashboard() {
                             </thead>
 
                             {address !== "" && (<tbody>
-                            <BankEntry tokenInternalId={0} tokenAddress={pigifyAddress} decimals={18} address={address} tokenName="PGY" image={USDT}/>
-                            <BankEntry tokenInternalId={1} tokenAddress={"0x5AB6F31B29Fc2021436B3Be57dE83Ead3286fdc7"} decimals={18} address={address} tokenName="USDT" image={USDT}/>
-                            <BankEntry tokenInternalId={2} tokenAddress={"0x466595626333c55fa7d7Ad6265D46bA5fDbBDd99"} decimals={18} address={address} tokenName="USDC" image={USDC}/>
+                            <BankEntry tokenInternalId={0} tokenAddress={pigifyAddress} decimals={18} address={address} tokenName="PGY" image={PGY}/>
+                            <BankEntry tokenInternalId={1} tokenAddress={"0x55d398326f99059fF775485246999027B3197955"} decimals={18} address={address} tokenName="USDT" image={USDT}/>
+                            <BankEntry tokenInternalId={2} tokenAddress={"0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"} decimals={18} address={address} tokenName="USDC" image={USDC}/>
+                            <BankEntry tokenInternalId={3} tokenAddress={"0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"} decimals={18} address={address} tokenName="BUSD" image={BUSD}/>
+                            <BankEntry tokenInternalId={4} tokenAddress={"0xd17479997F34dd9156Deef8F95A52D81D265be9c"} decimals={18} address={address} tokenName="USDD" image={USDD}/>
+                            <BankEntry tokenInternalId={4} tokenAddress={"0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3"} decimals={18} address={address} tokenName="DAI" image={DAI}/>
                             </tbody>)}
 
                         </table>
